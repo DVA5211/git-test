afd22b9448c93577746f6635b3a9ed818e98db09
张三 <zhangsan@itcast.cn>
2020-11-07 16:48:10 +0800

    first commit

---
+++ src/main/java/com/itheima/git/TestGit.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+/**
+ * @Author: ws
+ * @Date: 2020/11/7 0007 16:46
+ * @Version 1.0
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
