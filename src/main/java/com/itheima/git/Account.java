e05047c55605a9e5fbf8db0cb5735fa39848b097
张三 <zhangsan@itcast.cn>
2020-11-07 18:17:50 +0800

    修改注释

--- src/main/java/com/itheima/git/Account.java
+++ src/main/java/com/itheima/git/Account.java
@@ -2,11 +2,7 @@ package com.itheima.git;
 
 /**
  * @Author: ws
-<<<<<<< HEAD
  * @Date: 2020/11/7 0007 18:00
-=======
- * @Date: 2020/11/7 0007 17:55
->>>>>>> origin/master
  * @Version 1.0
  */
 public class Account {
