8b4b786aefad997bbfc9c3c19e52a615c4686120
张三 <zhangsan@itcast.cn>
2020-11-07 17:56:29 +0800

    test2_commit

---
+++ src/main/java/com/itheima/git/Account.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+/**
+ * @Author: ws
+ * @Date: 2020/11/7 0007 17:55
+ * @Version 1.0
+ */
+public class Account {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
