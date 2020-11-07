607aa745a86b793e88fc356f63a0095c6d38f490
张三 <zhangsan@itcast.cn>
2020-11-07 17:37:52 +0800

    test2_commit

--- src/main/java/com/itheima/git/TestGit.java
+++ src/main/java/com/itheima/git/TestGit.java
@@ -16,4 +16,10 @@ public class TestGit {
         System.out.println("hello git");
         System.out.println("hello git");
     }
+
+    public void test() {
+        int i = 0;
+        i += 10;
+        System.out.println(i);
+    }
 }
